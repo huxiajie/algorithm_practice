937857216e40b33cb70e90f140763a3dacb75252
jessie hu <huxiajie@outlook.com>
2019-12-15 18:58:16 +0800

    数据结构

---
+++ 数据结构与算法/object_operation.py
@@ -0,0 +1,16 @@
+'''
+0.list/set/tuple的操作效率较低
+1.新建空列表 O(1);新建含有n个元素的列表 O(n)
+2.l[0]=2 or l.append(2) O(n)
+'''
+def list_operation():
+    pass
+
+
+'''
+0.字典的操作效率较高
+1.添加新的键值对，平均复杂度为O(1)，最大复杂度为O(n)
+2.根据key访问value，平均复杂度为O(1)，最大复杂度为O(n)
+'''
+def dict_operation():
+    pass
